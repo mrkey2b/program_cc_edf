ed773632c91d31b6ab4b8d2bdbafc1c66de1c112
MrKey2B <mrkey2b@gmail.com>
2023-11-01 14:00:27 +0100

    Update global_db.lua

--- global_db.lua
+++ global_db.lua
@@ -43,11 +43,12 @@ local global_map = {
         Motor120v={0},
         Motor240v={1},
         Motor480v={2},
-        Motor960v={3}
+        Motor960v={3},
+        ComputerServerEDF={4}
     },
 
     pc_id_function = {
-        edf={0,1,2,3}
+        edf={0,1,2,3,4}
     },
 }
 return global_map
